90d4130652e274287ae3b1b062873de15b40a0b7
cpkal <haikal.putra.gustiansyah@upi.edu>
2025-03-10 12:15:25 +0700

    infinite scroll

--- src/components/BrandMarquee.js
+++ src/components/BrandMarquee.js
@@ -1,20 +1,30 @@
 import Image from "next/image"
+import './scroll.css'
 
 export default function BrandMarquee() {
   return (
-    <div>
-      <div className="flex gap-12 overflow-hidden">
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
-        <Image src={'https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png'} width={100} height={60} alt="Google Logo" />
+    <div className="scrolling-wrapper">
+      <div className="scrolling-content">
+        {/* Original Logos */}
+        {[...Array(10)].map((_, index) => (
+          <Image
+            key={index}
+            src="https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png"
+            width={100}
+            height={60}
+            alt="Google Logo"
+          />
+        ))}
+        {/* Duplicate Logos for Smooth Loop */}
+        {[...Array(10)].map((_, index) => (
+          <Image
+            key={`duplicate-${index}`}
+            src="https://white.logodownload.org/wp-content/uploads/2020/11/google-white-logo.png"
+            width={100}
+            height={60}
+            alt="Google Logo"
+          />
+        ))}
       </div>
     </div>
   )
